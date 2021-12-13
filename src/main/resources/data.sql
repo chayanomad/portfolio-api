30eff381952591b113bfead53a7599699bdabfa6
Riku Hashiki <riku.hashiki@ibm.com>
2021-12-13 21:33:34 +0900

    fix: データの不整合を修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,87 +1,87 @@
 INSERT INTO Products VALUES
-('AA000001', 'VEGETABLE', 'トマト', 'トマト', 80, '真っ赤でみずみずしい！', './img/products/tomato.jpg');
+('AA000001', 'VEGETABLE', 'とまと', 'トマト', 80, '真っ赤でみずみずしい！', './img/products/tomato.jpg');
 INSERT INTO Products VALUES
-('AA000002', 'VEGETABLE', 'ナス', 'ナス', 80, 'ふっくりきれいな！', './img/products/eggplant.jpg');
+('AA000002', 'VEGETABLE', 'なす', 'ナス', 80, 'ふっくりきれいな！', './img/products/eggplant.jpg');
 INSERT INTO Products VALUES
 ('AA000003', 'VEGETABLE', 'ピーマン', 'ピーマン', 80, '子供もおいしく食べれる！', './img/products/piman.jpg');
 INSERT INTO Products VALUES
-('AA000004', 'VEGETABLE', 'キュウリ', 'きゅうり', 80, 'サラダのお供に！', './img/products/cucamber.jpg');
+('AA000004', 'VEGETABLE', 'きゅうり', 'キュウリ', 80, 'サラダのお供に！', './img/products/cucamber.jpg');
 INSERT INTO Products VALUES
 ('AA000005', 'VEGETABLE', 'ズッキーニ', 'ズッキーニ', 600, 'おうち時間はピクルスを作ろう', './img/products/picrus.jpg');
 INSERT INTO Products VALUES
 ('AA000006', 'VEGETABLE', 'オクラ', 'オクラ', 200, '夏バテにはねばねば！', './img/products/okra.jpg');
 INSERT INTO Products VALUES
-('AA000007', 'VEGETABLE', 'トウモロコシ', 'とうもろこし', 300, '茹でても焼いても美味しい！', './img/products/corn.jpg');
+('AA000007', 'VEGETABLE', 'とうもろこし', 'トウモロコシ', 300, '茹でても焼いても美味しい！', './img/products/corn.jpg');
 INSERT INTO Products VALUES
 ('AA000008', 'VEGETABLE', 'バジル', 'バジル', 800, 'いつもの食事にひと手間工夫！', './img/products/basil.jpg');
 INSERT INTO Products VALUES
-('AA000009', 'VEGETABLE', 'シソ', 'しそ', 300, '手巻き寿司を作るあなたに！', './img/products/siso.jpg');
+('AA000009', 'VEGETABLE', 'しそ', 'シソ', 300, '手巻き寿司を作るあなたに！', './img/products/siso.jpg');
 INSERT INTO Products VALUES
 ('AA000010', 'VEGETABLE', 'モロヘイヤ', 'モロヘイヤ', 1500,'スムージーにお勧め', './img/products/moroheiya.jpg');
 INSERT INTO Products VALUES
-('AA000011', 'VEGETABLE', 'サヤエンドウ', 'さやエンドウ', 1000, 'ぷちぷち触感がたまらない！', './img/products/endo.jpg');
+('AA000011', 'VEGETABLE', 'さやえんどう', 'サヤエンドウ', 1000, 'ぷちぷち触感がたまらない！', './img/products/endo.jpg');
 INSERT INTO Products VALUES
-('AA000012', 'VEGETABLE', 'ソラマメ', 'そら豆', 1000, 'スーパーじゃ買えない！', './img/products/soramame.jpg');
+('AA000012', 'VEGETABLE', 'そら豆', 'ソラマメ', 1000, 'スーパーじゃ買えない！', './img/products/soramame.jpg');
 INSERT INTO Products VALUES
-('AA000013', 'VEGETABLE', 'エダマメ', '枝豆', 150, 'ビールのお供に', './img/products/edamame.jpg');
+('AA000013', 'VEGETABLE', '枝豆', 'エダマメ', 150, 'ビールのお供に', './img/products/edamame.jpg');
 INSERT INTO Products VALUES
-('AA000014', 'VEGETABLE', 'カボチャ', 'かぼちゃ', 200, 'BBQの定番！お子様も大好き', './img/products/pumpkin.jpg');
+('AA000014', 'VEGETABLE', 'かぼちゃ', 'カボチャ', 200, 'BBQの定番！お子様も大好き', './img/products/pumpkin.jpg');
 INSERT INTO Products VALUES
-('AA000015', 'VEGETABLE', 'ニンジン', '人参', 90, '使い方たくさん！', './img/products/carrot.jpg');
+('AA000015', 'VEGETABLE', '人参', 'ニンジン', 90, '使い方たくさん！', './img/products/carrot.jpg');
 INSERT INTO Products VALUES
-('AA000016', 'VEGETABLE', 'ジャガイモ', 'ジャガイモ', 80, '夏はじゃがバタ、冬はシチューに', './img/products/potato.jpg');
+('AA000016', 'VEGETABLE', 'じゃがいも', 'ジャガイモ', 80, '夏はじゃがバタ、冬はシチューに', './img/products/potato.jpg');
 INSERT INTO Products VALUES
-('AA000017', 'VEGETABLE', 'タマネギ', '玉ねぎ', 80, 'サラダにも炒め物にも万能野菜', './img/products/onion.jpg');
+('AA000017', 'VEGETABLE', '玉ねぎ', 'タマネギ', 80, 'サラダにも炒め物にも万能野菜', './img/products/onion.jpg');
 INSERT INTO Products VALUES
-('AA000018', 'VEGETABLE', 'ダイコン', '大根', 300, '甘くて美味しい', './img/products/daikon.jpg');
+('AA000018', 'VEGETABLE', '大根', 'ダイコン', 300, '甘くて美味しい', './img/products/daikon.jpg');
 INSERT INTO Products VALUES
-('AA000019', 'VEGETABLE', 'ネギ', 'ネギ', 100, 'そばにはやっぱりネギ', './img/products/negi.jpg');
+('AA000019', 'VEGETABLE', 'ねぎ', 'ネギ', 100, 'そばにはやっぱりネギ', './img/products/negi.jpg');
 INSERT INTO Products VALUES
-('AA000020', 'VEGETABLE', 'ゴボウ', 'ごぼう', 200, '豚汁を食べたくなったあなたへ', './img/products/gobo.jpg');
+('AA000020', 'VEGETABLE', 'ごぼう', 'ゴボウ', 200, '豚汁を食べたくなったあなたへ', './img/products/gobo.jpg');
 INSERT INTO Products VALUES
-('AA000021', 'VEGETABLE', 'サトイモ', '里芋', 500, '煮物にはこれが一番！', './img/products/satoimo.jpg');
+('AA000021', 'VEGETABLE', '里芋', 'サトイモ', 500, '煮物にはこれが一番！', './img/products/satoimo.jpg');
 INSERT INTO Products VALUES
 ('AA000022', 'VEGETABLE', 'キャベツ', 'キャベツ', 200, 'シャキシャキおいしい！', './img/products/cabbage.jpg');
 INSERT INTO Products VALUES
 ('AA000023', 'VEGETABLE', 'レタス', 'レタス', 400, 'ハンバーガーに挟んじゃおう！', './img/products/letace.jpg');
 INSERT INTO Products VALUES
-('AA000024', 'VEGETABLE', 'ハクサイ', '白菜', 300, '鍋にもってこい！', './img/products/hakusai.jpg');
+('AA000024', 'VEGETABLE', '白菜', 'ハクサイ', 300, '鍋にもってこい！', './img/products/hakusai.jpg');
 INSERT INTO Products VALUES
 ('AA000025', 'VEGETABLE', 'ブロッコリー', 'ブロッコリー', 700, 'マヨネーズ付けたら絶対おいしいよね', './img/products/burokkori.jpg');
 INSERT INTO Products VALUES
-('AA000026', 'VEGETABLE', 'ホウレンソウ', 'ほうれん草', 600, 'おひたしにどうぞ！', './img/products/spinach.jpg');
+('AA000026', 'VEGETABLE', 'ほうれん草', 'ホウレンソウ', 600, 'おひたしにどうぞ！', './img/products/spinach.jpg');
 INSERT INTO Products VALUES
-('AA000027', 'VEGETABLE', 'コマツナ', '小松菜', 400, 'おひたしにも、スムージーにも！', './img/products/komatsuna.jpg');
+('AA000027', 'VEGETABLE', '小松菜', 'コマツナ', 400, 'おひたしにも、スムージーにも！', './img/products/komatsuna.jpg');
 INSERT INTO Products VALUES
-('AA000028', 'VEGETABLE', 'カブ', 'かぶ', 1050, 'うんとこしょ、どっこいしょ！', './img/products/kabu.jpg');
+('AA000028', 'VEGETABLE', 'かぶ', 'カブ', 1050, 'うんとこしょ、どっこいしょ！', './img/products/kabu.jpg');
 INSERT INTO Products VALUES
-('AA000029', 'VEGETABLE', 'キセツノヤサイツメアワセセット', '季節の野菜詰め合わせセット', 2500, 'おいしい夏野菜がたっぷり！', './img/products/set.jpg');
+('AA000029', 'VEGETABLE', '季節の野菜詰め合わせセット', 'キセツノヤサイツメアワセセット', 2500, 'おいしい夏野菜がたっぷり！', './img/products/set.jpg');
 INSERT INTO Products VALUES
-('AA000030', 'FRUIT', 'リンゴ', 'りんご', 150, '果物といえばこれ！', './img/products/apple.jpg');
+('AA000030', 'FRUIT', 'りんご', 'リンゴ', 150, '果物といえばこれ！', './img/products/apple.jpg');
 INSERT INTO Products VALUES
-('AA000031', 'FRUIT', 'ナシ', 'なし', 150, 'みずみずしくてジューシー', './img/products/nasi.jpg');
+('AA000031', 'FRUIT', 'なし', 'ナシ', 150, 'みずみずしくてジューシー', './img/products/nasi.jpg');
 INSERT INTO Products VALUES
-('AA000032', 'FRUIT', 'ブドウ', 'ぶどう', 500, '皮も食べれる種無し', './img/products/grape.jpg');
+('AA000032', 'FRUIT', 'ぶどう', 'ブドウ', 500, '皮も食べれる種無し', './img/products/grape.jpg');
 INSERT INTO Products VALUES
-('AA000033', 'FRUIT', 'ミカン', 'みかん', 90, '冬のお供に', './img/products/orange.jpg');
+('AA000033', 'FRUIT', 'みかん', 'ミカン', 90, '冬のお供に', './img/products/orange.jpg');
 INSERT INTO Products VALUES
-('AA000034', 'FRUIT', 'イチゴ', 'いちご', 400, '女性に大人気！', './img/products/strawberry.jpg');
+('AA000034', 'FRUIT', 'いちご', 'イチゴ', 400, '女性に大人気！', './img/products/strawberry.jpg');
 INSERT INTO Products VALUES
-('AA000035', 'FRUIT', 'モモ', 'もも', 400, '今日は贅沢しませんか？', './img/products/peech.jpg');
+('AA000035', 'FRUIT', 'もも', 'モモ', 400, '今日は贅沢しませんか？', './img/products/peech.jpg');
 INSERT INTO Products VALUES
-('AA000036', 'FRUIT', 'サクランボ', 'さくらんぼ', 800, 'ひとくち感覚！', './img/products/cherry.jpg');
+('AA000036', 'FRUIT', 'さくらんぼ', 'サクランボ', 800, 'ひとくち感覚！', './img/products/cherry.jpg');
 INSERT INTO Products VALUES
-('AA000037', 'FRUIT', 'スイカ', 'スイカ', 1500, '夏の定番はコレ！', './img/products/watermelon.jpg');
+('AA000037', 'FRUIT', 'すいか', 'スイカ', 1500, '夏の定番はコレ！', './img/products/watermelon.jpg');
 INSERT INTO Products VALUES
-('AA000038', 'FRUIT', 'アンズ', 'アンズ', 2000, 'お酒にしてもおいしい！', './img/products/anzu.jpg');
+('AA000038', 'FRUIT', 'あんず', 'アンズ', 2000, 'お酒にしてもおいしい！', './img/products/anzu.jpg');
 INSERT INTO Products VALUES
 ('AA000039', 'FRUIT', 'プラム', 'プラム', 2000, '大人の味！', './img/products/plum.jpg');
 INSERT INTO Products VALUES
-('AA000040', 'JUICE', 'リンゴジュース', 'りんごジュース', 2000, 'すっきりジューシー！', './img/products/applejuice.jpg');
+('AA000040', 'JUICE', 'りんごジュース', 'リンゴジュース', 2000, 'すっきりジューシー！', './img/products/applejuice.jpg');
 INSERT INTO Products VALUES
 ('AA000041', 'JUICE', 'トマトジュース', 'トマトジュース', 800, 'たっぷり濃厚！', './img/products/tomatojuice.jpg');
 INSERT INTO Products VALUES
-('AA000042', 'JUICE', 'ブドウジュース', 'ぶどうジュース', 800, 'さっぱりおいしい！', './img/products/grapejuice.jpg');
+('AA000042', 'JUICE', 'ぶどうジュース', 'ブドウジュース', 800, 'さっぱりおいしい！', './img/products/grapejuice.jpg');
 
 INSERT INTO Stocks VALUES ('AA000001', 50);
 INSERT INTO Stocks VALUES ('AA000002', 10);
